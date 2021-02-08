76b8fc6f2487bcc5cc9b500c94d1f72ea647ac43
lmachens <leon@machens.koeln>
2021-02-08 11:28:10 +0100

    ➕ Add TypeScript support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2020: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 11,
     sourceType: "module",
   },
-  rules: {},
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
 };
