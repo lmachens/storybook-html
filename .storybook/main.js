14d463734d3fbc5693589858384e354f5716c936
lmachens <leon@machens.koeln>
2021-10-04 15:32:58 +0200

    ➕ Add postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ["../src/**/*.stories.[tj]s"],
   addons: [
+    "@storybook/addon-postcss",
     "@storybook/addon-viewport/register",
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
