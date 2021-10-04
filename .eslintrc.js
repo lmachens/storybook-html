7c11f8e421a89abf608efef559bce3f5ef4a9ccd
lmachens <leon@machens.koeln>
2021-10-04 10:57:47 +0200

    ➖ Remove unused dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,9 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
 };
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.[tj]s"],
   addons: [
-    "@storybook/preset-scss",
     "@storybook/addon-viewport/register",
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
